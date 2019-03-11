f491241590812a997f6d04ac54b2db57505337b1
Uros Lazarevic <enlision@gmail.com>
2019-03-11 23:16:32 +0100

    created data for user profile page and associated it with newly created userReducer

---
+++ src/data/mainInfo.js
@@ -0,0 +1,254 @@
+export default {
+  notifications: 8,
+  name: "author",
+  pseudonym: "space cowboy",
+  details: [
+    {
+      id: 1,
+      topic: "about me",
+      content: "lorem ipsum dolor sit amet, lorem ipsum dolor sit amet"
+    },
+    {
+      id: 2,
+      topic: "Favorite TV Shows",
+      content: "lorem ipsum dolor sit amet, lorem ipsum dolor sit amet"
+    },
+    {
+      id: 3,
+      topic: "Favorite Music Bands / Artists",
+      content: "lorem ipsum dolor sit amet, lorem ipsum dolor sit amet"
+    }
+  ],
+  socialNetworks: [
+    { id: 1, name: "facebook", src: "https://www.facebook.com/" },
+    { id: 2, name: "twitter", src: "https://twitter.com/?lang=en" },
+    { id: 1, name: "dribbble", src: "https://dribbble.com/" }
+  ],
+  photos: [
+    { id: 1, src: "https://loremflickr.com/320/240/nature?random=1" },
+    { id: 2, src: "https://loremflickr.com/320/240/nature?random=2" },
+    { id: 3, src: "https://loremflickr.com/320/240/nature?random=3" },
+    { id: 4, src: "https://loremflickr.com/320/240/nature?random=4" },
+    { id: 5, src: "https://loremflickr.com/320/240/nature?random=5" },
+    { id: 6, src: "https://loremflickr.com/320/240/nature?random=6" },
+    { id: 7, src: "https://loremflickr.com/320/240/nature?random=7" },
+    { id: 8, src: "https://loremflickr.com/320/240/nature?random=8" },
+    { id: 9, src: "https://loremflickr.com/320/240/nature?random=9" }
+  ],
+  friends: {
+    total: 28,
+    list: [
+      {
+        id: 1,
+        src: "https://loremflickr.com/320/240/people?random=1",
+        badgeColor: "green",
+        name: "lorem"
+      },
+      {
+        id: 2,
+        src: "https://loremflickr.com/320/240/people?random=2",
+        badgeColor: "red",
+        name: "lorem"
+      },
+      {
+        id: 3,
+        src: "https://loremflickr.com/320/240/people?random=3",
+        badgeColor: "blue",
+        name: "lorem"
+      },
+      {
+        id: 4,
+        src: "https://loremflickr.com/320/240/people?random=4",
+        badgeColor: "yellow",
+        name: "lorem"
+      },
+      {
+        id: 5,
+        src: "https://loremflickr.com/320/240/people?random=5",
+        badgeColor: "gray",
+        name: "lorem"
+      },
+      {
+        id: 6,
+        src: "https://loremflickr.com/320/240/people?random=6",
+        badgeColor: "gray",
+        name: "lorem"
+      }
+    ]
+  },
+  badges: [
+    {
+      id: 1,
+      src: "https://loremflickr.com/320/240/random?random=1",
+      number: 1
+    },
+    {
+      id: 2,
+      src: "https://loremflickr.com/320/240/random?random=2",
+      number: ""
+    },
+    {
+      id: 3,
+      src: "https://loremflickr.com/320/240/random?random=3",
+      number: ""
+    },
+    {
+      id: 4,
+      src: "https://loremflickr.com/320/240/random?random=4",
+      number: ""
+    },
+    {
+      id: 5,
+      src: "https://loremflickr.com/320/240/random?random=5",
+      number: 1
+    },
+    {
+      id: 6,
+      src: "https://loremflickr.com/320/240/random?random=6",
+      number: ""
+    },
+    {
+      id: 7,
+      src: "https://loremflickr.com/320/240/random?random=7",
+      number: ""
+    },
+    {
+      id: 8,
+      src: "https://loremflickr.com/320/240/random?random=8",
+      number: 2
+    },
+    { id: 9, src: "https://loremflickr.com/320/240/random?random=9", number: 2 }
+  ],
+  spotifyList: [
+    {
+      id: 1,
+      src: "https://loremflickr.com/320/240/random?random=1",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    },
+    {
+      id: 2,
+      src: "https://loremflickr.com/320/240/random?random=2",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    },
+    {
+      id: 3,
+      src: "https://loremflickr.com/320/240/random?random=3",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    },
+    {
+      id: 4,
+      src: "https://loremflickr.com/320/240/random?random=4",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    },
+    {
+      id: 5,
+      src: "https://loremflickr.com/320/240/random?random=5",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    }
+  ],
+  twitterFeed: [
+    {
+      id: 1,
+      pseudonym: "Space Cowboy",
+      src: "https://loremflickr.com/320/240/random",
+      author: "author",
+      tags: ["tag1", "tag2"]
+    },
+    {
+      id: 2,
+      pseudonym: "Space Cowboy",
+      src: "https://loremflickr.com/320/240/random",
+      author: "author",
+      tags: ["tag1", "tag2"]
+    },
+    {
+      id: 3,
+      pseudonym: "Space Cowboy",
+      src: "https://loremflickr.com/320/240/random",
+      author: "author",
+      tags: ["tag1", "tag2"]
+    },
+    {
+      id: 4,
+      pseudonym: "Space Cowboy",
+      src: "https://loremflickr.com/320/240/random",
+      author: "author",
+      tags: ["tag1", "tag2"]
+    },
+    {
+      id: 5,
+      pseudonym: "Space Cowboy",
+      src: "https://loremflickr.com/320/240/random",
+      author: "author",
+      tags: ["tag1", "tag2"]
+    }
+  ],
+  blogs: [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet",
+      body: "Lorem ipsum dolor sit amet lorem ipsum dolor",
+      date: "March 18th 2018",
+      time: "19:00h"
+    },
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet",
+      body: "Lorem ipsum dolor sit amet lorem ipsum dolor",
+      date: "March 18th 2018",
+      time: "19:00h"
+    }
+  ],
+  favPages: [
+    {
+      id: 1,
+      src: "https://loremflickr.com/320/240/random?random=1",
+      title: "Lorem ipsum Dolor",
+      description: "Some description about song"
+    }
+  ],
+  pools: [
+    {
+      id: 1,
+      question: "Lorem ipsum dolor?",
+      totalVotes: 9,
+      answers: [
+        {
+          id: 1,
+          answer: "Lorem ipsum",
+          votes: 4,
+          voters: [
+            { id: 1, src: "https://loremflickr.com/320/240/people?random=1" },
+            { id: 2, src: "https://loremflickr.com/320/240/people?random=2" },
+            { id: 3, src: "https://loremflickr.com/320/240/people?random=3" },
+            { id: 4, src: "https://loremflickr.com/320/240/people?random=4" }
+          ]
+        },
+        {
+          id: 2,
+          answer: "Lorem ipsum dolor",
+          votes: 3,
+          voters: [
+            { id: 5, src: "https://loremflickr.com/320/240/people?random=5" },
+            { id: 6, src: "https://loremflickr.com/320/240/people?random=6" },
+            { id: 7, src: "https://loremflickr.com/320/240/people?random=7" }
+          ]
+        },
+        {
+          id: 3,
+          answer: "Lorem ipsum dolor sit amet",
+          votes: 2,
+          voters: [
+            { id: 8, src: "https://loremflickr.com/320/240/people?random=8" },
+            { id: 9, src: "https://loremflickr.com/320/240/people?random=9" }
+          ]
+        }
+      ]
+    }
+  ]
+};
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+import mainInfo from "/data/mainInfo";
+
+const initialState = {
+  basicInfo: {},
+  mainInfo
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
