1fc0e02f46629a48847ba44c7fd21716c6fb3a8c
Uros <uros.lazarevic.la@gmail.com>
2019-07-20 14:46:49 +0200

    Fix component folder names step 1

--- src/components/profileT/Content/Main/Details/Right/UserPool/Item/Answer/Answer.js
+++ src/components/profileT/Content/Main/Details/Right/UserPool/Item/Answer/Answer.js
@@ -4,15 +4,8 @@ const Answer = props => {
   return (
     <div className="answer">
       <label htmlFor="answer">
-        <span
-          id={props.radioBtnId}
-          onClick={props.handlePoolVoting}
-          className="radio-btn"
-          value={props.answer}
-        >
-          {props.clickedRadioBtnId === props.radioBtnId && (
-            <p className="radio-btn-target" />
-          )}
+        <span id={props.radioBtnId} onClick={props.handlePoolVoting} className="radio-btn" value={props.answer}>
+          {props.clickedRadioBtnId === props.radioBtnId && <p className="radio-btn-target" />}
         </span>
         {props.answer}
       </label>
